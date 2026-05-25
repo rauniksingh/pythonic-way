101d00d89ea8e372dd56755ce209694e8c0de531
rauniksingh <rauniksingh@outlook.com>
2026-05-25 16:42:55 +0530

    Day-18: create colorful dot art using Turtle and RGB tuples

---
+++ day-18/print-dots.py
@@ -0,0 +1,33 @@
+import turtle as turtle_module
+import random
+from rgb_color_list import color_list
+
+turtle_module.colormode(255)
+
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
+tim.setheading(270)
+tim.forward(300)
+tim.setheading(0)
+
+number_of_dots = 100
+
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+screen = turtle_module.Screen()
+screen.exitonclick()
